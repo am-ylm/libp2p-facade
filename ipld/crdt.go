67fd083d24c07e19a0c5b4acac135a265244cf76
Amir Yahalom <amir.ayd@gmail.com>
2020-09-15 15:23:44 +0300

    add DAGSyncer instead of using ipldNode

--- ipld/crdt.go
+++ ipld/crdt.go
@@ -2,15 +2,14 @@ package ipld
 
 import (
 	"github.com/amirylm/priv-libp2p-node/core"
+	"github.com/ipfs/go-cid"
 	ds "github.com/ipfs/go-datastore"
 	crdt "github.com/ipfs/go-ds-crdt"
+	blockstore "github.com/ipfs/go-ipfs-blockstore"
+	ipld "github.com/ipfs/go-ipld-format"
 )
 
-type CrdtConfig struct {
-	TopicName string
-}
-
-func ConfigureCrdt(node IpldNode, crdtOpts *crdt.Options, topicName string) (*crdt.Datastore, error) {
+func ConfigureCrdt(node IpldNode, topicName string, crdtOpts *crdt.Options) (*crdt.Datastore, error) {
 	if crdtOpts == nil {
 		crdtOpts = crdt.DefaultOptions()
 	}
@@ -22,7 +21,8 @@ func ConfigureCrdt(node IpldNode, crdtOpts *crdt.Options, topicName string) (*cr
 		node.Logger().Fatalf("could not create crdt.PubSubBroadcaster: %s", err.Error())
 	}
 
-	crdt, err := crdt.New(node.Store(), ds.NewKey("crdt"), node.(*ipldNode), pubsubBC, crdtOpts)
+	dsync := NewDagSyncer(node.DagService(), node.BlockService().Blockstore())
+	crdt, err := crdt.New(node.Store(), ds.NewKey("crdt"), dsync, pubsubBC, crdtOpts)
 	if err != nil {
 		node.Logger().Fatal(err)
 	}
@@ -30,3 +30,20 @@ func ConfigureCrdt(node IpldNode, crdtOpts *crdt.Options, topicName string) (*cr
 
 	return crdt, err
 }
+
+func NewDagSyncer(base ipld.DAGService, bs blockstore.Blockstore) *dagSyncer {
+	n := dagSyncer{base, bs}
+
+	return &n
+}
+
+type dagSyncer struct {
+	ipld.DAGService
+
+	bs blockstore.Blockstore
+}
+
+func (n dagSyncer) HasBlock(c cid.Cid) (bool, error) {
+	return n.bs.Has(c)
+}
+
